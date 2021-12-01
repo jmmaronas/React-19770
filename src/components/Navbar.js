dca4dba8815c2f69ad2fe6ddf4e9a36b02efa725
Juan Manuel Maroñas <jmmaronas@gmail.com>
2021-11-30 22:37:58 -0300

    Clase-3

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+
+function Navbar(){
+    return(
+        <div className="head">
+            <div className="titulo">
+                <h1>Pagina de practica</h1>
+                <img src="./logo192.png"/>
+            </div>
+            <div>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Productos</li>
+                    <li>Contacto</li>
+                    <li>Carrito</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
